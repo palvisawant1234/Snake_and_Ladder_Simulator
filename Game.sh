7e139c2b38a74dc12dacd417aebc2ed4244fa417
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-20 23:28:25 +0530

    Added:Functionality to check options Noplay Snake or Ladder

--- Game.sh
+++ Game.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
 
 POSITION=0
+option(){
+	CHOICE=$((RANDOM % 3))
+	echo $CHOICE
+	if [ $CHOICE -eq 2 ]
+	then
+		echo "Ladder"
+		POSITION=$(($POSITION + $dieRoll))
+	elif [ $CHOICE -eq 1 ]
+	then
+		echo "Snake"
+		POSITION=$(($POSITION - $dieRoll))
+	else
+		echo "No play"
+	fi
+}
 dice()
 {
-	dieRoll=$((RANDOM % 6 + 1))
-	echo "You got $dieRoll"
+        dieRoll=$((RANDOM % 6 + 1))
+        echo "You got $dieRoll"
+	option
 }
 dice
+echo Your position is:$POSITION
