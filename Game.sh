e1c769c54e7bff6514435290a21ff68eebdb4e13
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-25 03:10:42 +0530

    Added:comments for better understanding

--- Game.sh
+++ Game.sh
@@ -1,8 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
 
+#variables
 POSITION=0
 player1position=0
 player2position=0
+count=1
+counter=1
+
+declare -a playerOne
+declare -a playerTwo
+
+playerOne[0]=0
+playerTwo[0]=0
+
+#roll a die and check the option snake,ladder or no play
 die(){
 	dieRoll=$((RANDOM % 6 + 1))
         echo "You got : $dieRoll"
@@ -21,6 +32,8 @@ die(){
 		echo "No play"
 	fi
 }
+
+#check condition for above ,below and equal 100
 checkCondition(){
 	if [ $POSITION -lt 0 ]
 	then
@@ -38,6 +51,8 @@ checkCondition(){
 	fi
 	echo Your position is:$POSITION
 }
+
+#start the game
 playGame(){
 	while [ $POSITION -ne 100 ]
 	do
@@ -47,14 +62,18 @@ playGame(){
 			POSITION=$player1position
 			die
 			checkCondition
+			playerOne[count]=$POSITION
 			player1position=$POSITION
 			player=2
+			count=$(($count+1))
 		else
 			POSITION=$player2position
 			die
 			checkCondition
+			playerTwo[counter]=$POSITION
 			player2position=$POSITION
 			player=1
+			counter=$(($counter+1))
 		fi
 	done
 }
